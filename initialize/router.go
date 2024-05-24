5aeec6965fbc0ed5df1a54c84bab611f19a7f360
ohwin <ohwin520@gmail.com>
2024-05-24 23:00:33 +0800

    init

--- initialize/router.go
+++ initialize/router.go
@@ -8,8 +8,8 @@ import (
 func Routers(r *gin.Engine) {
 	//r = r.Group("", middleware.CORS())
 	r.Use(middleware.CORS())
-	routerPublic := r.Group("")
-	routerProtect := r.Group("", middleware.JWTAuth())
+	//routerPublic := r.Group("")
+	//routerProtect := r.Group("", middleware.JWTAuth())
 	//ro.InitBaseRouter(routerPublic)
 	//router.InitUserRouter(routerProtect)
 	//router.InitQuestionRouter(routerPublic)
